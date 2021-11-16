8596f598a14a9200cb9ff3ddb1ac534ad2e31fcb
BayuAjiNugraha11687 <90235493+BayuAjiNugraha11687@users.noreply.github.com>
2021-11-16 10:33:42 +0700

    UTS

---
+++ UTS/UTS/Assets/Script/GenerateObs.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GenerateObs : MonoBehaviour
+{
+    public GameObject rocks;
+    int score = 0;
+    public Text ScoreTxt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("CreateObstacle", 1f, 1.5f);
+    }
+
+    void CreateObstacle()
+    {
+        Instantiate (rocks);
+        score++;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        ScoreTxt.text = "Score : " + score.ToString();
+    }
+}
